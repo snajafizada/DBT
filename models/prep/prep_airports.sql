736380f1a835dd4ab581c08b5e89409f84470aa0
snajafizada <snajafizada@gmail.com>
2025-03-11 16:53:48 +0100

    updated flights and airports files

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,15 +1,14 @@
 WITH airports_reorder AS (
-    SELECT faa
-           ,airport_name
-           ,city
-           ,country
-           ,region  -- Reorder: region comes after country
-           ,latitude
-           ,longitude
-           ,elevation
-           ,timezone
-           ,iso_country
-           ,iso_region
+    SELECT faa AS 
+           ,name AS airport_name
+           ,city AS city
+           ,country AS country -- Keep country here
+           ,region AS region  -- Reorder: region comes after country
+           ,lat AS latitude
+           ,lon AS longitude
+           ,alt AS elevation
+           ,tz AS timezone
+           ,dst AS destination
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
--- models/prep/prep_flights.sql
+++ models/prep/prep_flights.sql
@@ -3,27 +3,28 @@ WITH flights_one_month AS (
     FROM {{ref('staging_flights_one_month')}}
 ),
 flights_cleaned AS(
-    SELECT flight_date::DATE
-           ,TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time
-           ,TO_CHAR(sched_dep_time, 'fm0000')::TIME AS sched_dep_time  -- Cast scheduled departure time as TIME
-           ,dep_delay
-           ,(dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
-           ,TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time  -- Cast arrival time as TIME
-           ,TO_CHAR(sched_arr_time, 'fm0000')::TIME AS sched_arr_time  -- Cast scheduled arrival time as TIME
-           ,arr_delay
-           ,(arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval  -- Convert arrival delay to INTERVAL
-           ,airline
-           ,tail_number
-           ,flight_number
-           ,origin
-           ,dest
-           ,air_time
-           ,(air_time * '1 minute'::INTERVAL) AS air_time_interval  -- Convert air time to INTERVAL
-           ,actual_elapsed_time
-           ,(actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval  -- Convert actual elapsed time to INTERVAL
-           ,(distance * 1.60934)::NUMERIC(6,2) AS distance_km  -- Convert distance from miles to kilometers
-           ,cancelled
-           ,diverted
+    SELECT 
+        flight_date::DATE,
+        TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time, -- Format as TIME
+        TO_CHAR(sched_dep_time, 'fm0000')::TIME AS sched_dep_time, -- Scheduled departure time
+        dep_delay,
+        (dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval, -- Convert dep_delay to INTERVAL
+        TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time, -- Arrival time
+        TO_CHAR(sched_arr_time, 'fm0000')::TIME AS sched_arr_time, -- Scheduled arrival time
+        arr_delay,
+        (arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval, -- Convert arr_delay to INTERVAL
+        airline,
+        tail_number,
+        flight_number,
+        origin,
+        dest,
+        air_time,
+        (air_time * '1 minute'::INTERVAL) AS air_time_interval, -- Convert air_time to INTERVAL
+        actual_elapsed_time,
+        (actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval, -- Convert actual_elapsed_time to INTERVAL
+        (distance * 0.621371)::NUMERIC(6,2) AS distance_km, -- Convert distance to kilometers
+        cancelled,
+        diverted
     FROM flights_one_month
 )
 SELECT * FROM flights_cleaned
