70b67adc86d7bafd467de5a5cdaa74addc642cba
snajafizada <snajafizada@gmail.com>
2025-03-11 16:37:46 +0100

    updated flights and airports files

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,7 +1,15 @@
 WITH airports_reorder AS (
     SELECT faa
-    	   ,...
-    	   ...
+           ,airport_name
+           ,city
+           ,country
+           ,region  -- Reorder: region comes after country
+           ,latitude
+           ,longitude
+           ,elevation
+           ,timezone
+           ,iso_country
+           ,iso_region
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
