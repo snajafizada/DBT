4532feabbbe66bab53a985308c718882e46bc873
snajafizada <snajafizada@gmail.com>
2025-03-11 16:59:37 +0100

    updated the flights and airports files

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,14 +1,14 @@
 WITH airports_reorder AS (
-    SELECT faa AS 
-           ,name AS airport_name
-           ,city AS city
-           ,country AS country -- Keep country here
-           ,region AS region  -- Reorder: region comes after country
-           ,lat AS latitude
-           ,lon AS longitude
-           ,alt AS elevation
-           ,tz AS timezone
-           ,dst AS destination
-    FROM {{ref('staging_airports')}}
+    SELECT faa,
+           name AS airport_name,
+           city,
+           country, -- Keep country here
+           region,  -- Reorder: region comes after country
+           lat AS latitude,
+           lon AS longitude,
+           alt AS elevation,
+           tz AS timezone,
+           dst AS destination
+    FROM "lavender_notebooks"."s_saedanajafizada"."staging_airports"
 )
 SELECT * FROM airports_reorder
